c2488fa05fedc57da90a9693a45b4cf713cd3dd6
Stephen Folorunsho <107089079+Stephen-Heff@users.noreply.github.com>
2023-06-12 13:03:21 -0400

    Update PostingDetails.jsx

--- src/routes/PostingDetails.jsx
+++ src/routes/PostingDetails.jsx
@@ -69,46 +69,6 @@ export default function PostingDetails() {
           {posting.email}
         </a>
         </div>
-//         <div>
-//           <button className="apply-btn" onClick={handleApplyClick}>
-//             Apply
-//           </button>
-//         </div>
-//         {showApplyText && (
-//           <section className="apply-section content-wrapper" id="apply">
-//             <h2>Job Application</h2>
-
-//             <div className="apply-flexbox">
-//               <form method="get">
-//                 <div className="apply-container">
-//                   <div>
-//                     <label htmlFor="name">Your Name:</label>
-//                     <input type="text" id="name" name="name" placeholder="" />
-//                   </div>
-//                   <div>
-//                     <label htmlFor="email">Email Address:</label>
-//                     <input type="email" id="email" name="email" />
-//                   </div>
-//                   <div>
-//                     <label htmlFor="phone">Phone:</label>
-//                     <input type="number" id="phone" name="phone" />
-//                   </div>
-//                   <div>
-//                     <h6>Upload your resume</h6>
-//                     <input
-//                       className="file-input"
-//                       type="file"
-//                       onChange={handleFileChange}
-//                     />
-//                   </div>
-//                   <button className="apply-btn" type="submit">
-//                     Submit
-//                   </button>
-//                 </div>
-//               </form>
-//             </div>
-//           </section>
-//         )}
       </div>
     </section>
   );
